5f04f8512086a3f5a0a4a4f1bc9c675fbd298c2b
Maxim Filipovich <fatumka@gmail.com>
2020-03-29 13:58:44 +0300

    Fix naming issue

--- api/loan/policy.ts
+++ api/loan/policy.ts
@@ -2,7 +2,7 @@ import { policy, User } from "../../operation/policy";
 import { State } from "../../operation";
 
 const adminPolicy = policy({
-  isCan(user: User, state: State) {
+  can(user: User, state: State) {
     return user.roles.includes("admin");
   },
 });
--- operation/policy.ts
+++ operation/policy.ts
@@ -1,11 +1,11 @@
 import { State, step, ok, fail } from ".";
 
 type User = { roles: string[] };
-type Policy = { isCan(user: User, state: State): boolean };
+type Policy = { can(user: User, state: State): boolean };
 
 const policy = (policy: Policy) => {
   return step("policy", (state: State) => {
-    if (policy.isCan(state.user, state)) {
+    if (policy.can(state.user, state)) {
       return ok(state);
     } else {
       return fail("error");
